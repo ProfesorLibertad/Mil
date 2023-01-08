95577c681b077a56b6f2b3628e7db91113d5f93a
ProfesorLibertad <61976537+ProfesorLibertad@users.noreply.github.com>
2023-01-08 10:37:31 -0500

    Delete scri.js

--- scri.js
+++
@@ -1,5 +0,0 @@
-//Entrada de datos
-number = prompt("Introduzca decimal");
-
-//convertir a binario
-function convertToBinary1 (number) { let num = number; let binary = (num % 2).toString(); for (; num > 1; ) { num = parseInt(num / 2); binary = (num % 2) + (binary); } console.log(binary); } window.onload = function () { console.log(convertToBinary1(number)); }
